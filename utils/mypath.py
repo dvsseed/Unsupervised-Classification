a91fbb25d7fd66d12633aebe7990925246e0b548
曾令燊 <dvsseed@hotmail.com>
2020-08-09 09:25:15 +0800

    Update mypath.py

--- utils/mypath.py
+++ utils/mypath.py
@@ -12,16 +12,16 @@ class MyPath(object):
         assert(database in db_names)
 
         if database == 'cifar-10':
-            return '/path/to/cifar-10/'
+            return '/content/Unsupervised-Classification/data/cifar-10/'
         
         elif database == 'cifar-20':
-            return '/path/to/cifar-20/'
+            return '/content/Unsupervised-Classification/data/cifar-20/'
 
         elif database == 'stl-10':
-            return '/path/to/stl-10/'
+            return '/content/Unsupervised-Classification/data/stl-10/'
         
         elif database in ['imagenet', 'imagenet_50', 'imagenet_100', 'imagenet_200']:
-            return '/path/to/imagenet/'
+            return '/content/Unsupervised-Classification/data/imagenet/'
         
         else:
             raise NotImplementedError
